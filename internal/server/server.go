67fbe9163f5788d5808b41a5cf66872aa91baead
Erazem Kokot <mail@erazem.eu>
2022-12-03 23:41:15 +0100

    Update endpoints

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,32 +3,59 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/multimoml/dispatcher/internal/config"
 	"github.com/multimoml/dispatcher/internal/database"
 )
+
+var dbClient *mongo.Client
 
 func Run(ctx context.Context) {
 	// Load environment variables
 	config.Environment()
 
 	// Connect to MongoDB
-	database.Connect(ctx)
+	dbClient = database.Connect(ctx)
 
 	// Start HTTP server
 	router := httprouter.New()
 
 	// Endpoints
-	router.GET("/v1/products", Products)
+	router.GET("/live", Liveliness)
+	router.GET("/ready", Readiness)
+	router.GET("/v1/all", AllProducts)
 
 	log.Fatal(http.ListenAndServe(":6001", router))
 }
 
-func Products(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+func Liveliness(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	if _, err := fmt.Fprint(w, "I'm alive!\n"); err != nil {
+		log.Println(err)
+	}
+}
+
+func Readiness(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	if err := dbClient.Ping(context.TODO(), nil); err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		if _, err = fmt.Fprint(w, "I'm NOT ready!\n"); err != nil {
+			log.Println(err)
+		}
+	} else {
+		w.WriteHeader(http.StatusOK)
+		if _, err = fmt.Fprint(w, "I'm ready!\n"); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func AllProducts(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	products := database.Products(context.TODO())
 
 	w.Header().Set("Content-Type", "application/json")
